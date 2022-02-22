cd2830033e4fb992cb77716ad474b79f90495732
JaeCheon <studyJcHub@gmail.com>
2022-02-22 14:42:09 +0900

    Serializable 을 활용하여, 클릭 된 스토어 정보를 첨부  - 화면 이동

--- app/src/main/java/com/jc/pizzadeliverywithteacher/datas/StoreData.kt
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/datas/StoreData.kt
@@ -1,9 +1,11 @@
 package com.jc.pizzadeliverywithteacher.datas
 
+import java.io.Serializable
+
 class StoreData(
     val name: String,
     val rating: Double,
     val phoneNum: String,
     val logoImageURL: String
-) {
+) : Serializable {
 }
