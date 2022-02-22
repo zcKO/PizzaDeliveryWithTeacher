d66777157ac5a8e7e62d1cfa5284f980ceb28975
JaeCheon <studyJcHub@gmail.com>
2022-02-22 15:20:44 +0900

    전화 주문 버튼 클릭 > 권한 물어보는 기능만 구현

--- app/src/main/java/com/jc/pizzadeliverywithteacher/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/ViewStoreDetailActivity.kt
@@ -1,8 +1,12 @@
 package com.jc.pizzadeliverywithteacher
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.jc.pizzadeliverywithteacher.datas.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
@@ -36,7 +40,29 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
     fun setUpEvents() {
 
+//        권한 관련 변수를, 화면이 만들어질 때 한번만 세팅
+        val pl = object : PermissionListener {
+            override fun onPermissionGranted() {
 
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@ViewStoreDetailActivity, "통화 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
+        btnCall.setOnClickListener {
+//            클릭 될 때마다 만든 변수를 사용 만 한다.
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
 
     }
 
