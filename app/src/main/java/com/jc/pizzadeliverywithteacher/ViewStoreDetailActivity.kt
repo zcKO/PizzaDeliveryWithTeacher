dfcf14179c5b9cc582c4ef686c8bfe53ce6687e0
JaeCheon <studyJcHub@gmail.com>
2022-02-22 14:46:43 +0900

    첨부해준 데이터 받아오기  - 맴버 변수로 만들어서 활용

--- app/src/main/java/com/jc/pizzadeliverywithteacher/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/ViewStoreDetailActivity.kt
@@ -1,11 +1,30 @@
 package com.jc.pizzadeliverywithteacher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jc.pizzadeliverywithteacher.datas.StoreData
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+
+    lateinit var mStoreData: StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+
+        mStoreData = intent.getSerializableExtra("store") as StoreData
+
+        setValues()
+        setUpEvents()
+
     }
+
+    fun setValues() {
+
+    }
+
+    fun setUpEvents() {
+
+    }
+
 }
