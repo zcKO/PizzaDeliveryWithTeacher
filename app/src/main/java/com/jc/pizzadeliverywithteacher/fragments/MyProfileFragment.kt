aa286aafff9b830bb37519206261ca86cc4192c5
JaeCheon <studyJcHub@gmail.com>
2022-02-22 15:58:58 +0900

    프래그먼트로 돌아왔을 때, 첨부한 닉네임 반영

--- app/src/main/java/com/jc/pizzadeliverywithteacher/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.jc.pizzadeliverywithteacher.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -42,6 +43,21 @@ class MyProfileFragment: Fragment() {
 
             val myIntent = Intent(requireContext(), EditNicknameActivity::class.java)
             startActivityForResult(myIntent, REQ_CODE_NICKNAME)
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_CODE_NICKNAME) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val nickname = data?.getStringExtra("nick")
+                txtNickname.text = nickname
+            }
 
         }
 
