317d7a225f0d9ed8f78cd1a37baf4b98ce97e084
JaeCheon <studyJcHub@gmail.com>
2022-02-22 10:04:49 +0900

    ViewPagerAdapter 와 ViewPager 연결 작업  - 실행 결과로 화면 이동 가능

--- app/src/main/java/com/jc/pizzadeliverywithteacher/MainActivity.kt
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.jc.pizzadeliverywithteacher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jc.pizzadeliverywithteacher.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewPagerAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setValues()
+        setUpEvents()
+
     }
+
+    fun setValues() {
+
+
+        mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mViewPagerAdapter
+
+
+    }
+
+    fun setUpEvents() {
+
+    }
+
 }
