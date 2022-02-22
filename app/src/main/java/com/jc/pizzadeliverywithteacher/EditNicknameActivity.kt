87c16b2e369f82d52676628cd0c843fd294ba10d
JaeCheon <studyJcHub@gmail.com>
2022-02-22 15:45:42 +0900

    닉네임 변경 화면 추가만

---
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/EditNicknameActivity.kt
@@ -0,0 +1,24 @@
+package com.jc.pizzadeliverywithteacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+
+        setValues()
+        setUpEvents()
+
+    }
+
+    fun setValues() {
+
+    }
+
+    fun setUpEvents() {
+
+    }
+
+}
