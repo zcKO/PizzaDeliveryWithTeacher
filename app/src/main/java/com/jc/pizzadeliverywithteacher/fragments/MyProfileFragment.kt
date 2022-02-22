d53780cd8b088026b0e79fc365c75a3b8d4659ef
JaeCheon <studyJcHub@gmail.com>
2022-02-22 09:50:15 +0900

    Fragment class (My Profile) 추가

---
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/fragments/MyProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.jc.pizzadeliverywithteacher.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jc.pizzadeliverywithteacher.R
+
+class MyProfileFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        todo 내 정보 화면의 동작 코드
+    }
+
+}
