bab8f603b6d035107e44bfd1760d14f5fed23cc2
JaeCheon <studyJcHub@gmail.com>
2022-02-22 09:58:53 +0900

    getCount() / getItem() 오버라이딩하여 내용 구현

--- app/src/main/java/com/jc/pizzadeliverywithteacher/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/adapters/MainViewPagerAdapter.kt
@@ -1,10 +1,24 @@
 package com.jc.pizzadeliverywithteacher.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.jc.pizzadeliverywithteacher.fragments.MyProfileFragment
+import com.jc.pizzadeliverywithteacher.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(
     fm: FragmentManager,
 ) : FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int = 2
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position) {
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
+
+    }
 
 }
