7af77ea843b5e9e3b6112a3504fb8b002049f455
JaeCheon <studyJcHub@gmail.com>
2022-02-22 11:02:16 +0900

    getView() 함수 기본 구조

--- app/src/main/java/com/jc/pizzadeliverywithteacher/adapters/StoreAdapter.kt
+++ app/src/main/java/com/jc/pizzadeliverywithteacher/adapters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.jc.pizzadeliverywithteacher.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.jc.pizzadeliverywithteacher.R
 import com.jc.pizzadeliverywithteacher.datas.StoreData
 
 class StoreAdapter(
@@ -9,5 +13,18 @@ class StoreAdapter(
     resId: Int,
     val storeList: ArrayList<StoreData>
 ): ArrayAdapter<StoreData>(mContext, resId, storeList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 
 }
